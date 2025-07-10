7658200b6cf49f27c0ee4c57be49be5ad678d632
afarghaly10 <sami.farghaly@gmail.com>
2025-07-10 10:54:28 -0400

    chat history addition

--- doc-assistant/backend/core.py
+++ doc-assistant/backend/core.py
@@ -1,32 +1,39 @@
 import os
 from dotenv import load_dotenv
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
-from langchain import hub
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_pinecone import PineconeVectorStore
-
 
 load_dotenv()
 
-index = os.environ["INDEX_NAME"]
+
+from typing import Any, Dict, List
+
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+INDEX_NAME = os.environ["INDEX_NAME"]
 
 
-def run_llm(query: str):
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-    docsearch = PineconeVectorStore(index_name=index, embedding=embeddings)
+    docsearch = PineconeVectorStore(index_name=INDEX_NAME, embedding=embeddings)
     chat = ChatOpenAI(verbose=True, temperature=0)
+
+    rephrase_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
 
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
     stuff_documents_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
 
-    # Retrieval
+    history_aware_retriever = create_history_aware_retriever(
+        llm=chat, retriever=docsearch.as_retriever(), prompt=rephrase_prompt
+    )
     qa = create_retrieval_chain(
-        retriever=docsearch.as_retriever(),
-        combine_docs_chain=stuff_documents_chain,
+        retriever=history_aware_retriever, combine_docs_chain=stuff_documents_chain
     )
 
-    result = qa.invoke({"input": query})
+    result = qa.invoke(input={"input": query, "chat_history": chat_history})
     response = {
         "query": result["input"],
         "result": result["answer"],
--- doc-assistant/main.py
+++ doc-assistant/main.py
@@ -5,10 +5,14 @@ st.header("LangChain Documentation Assistant")
 
 prompt = st.text_input("Prompt", placeholder="Enter your prompt")
 
-if "user_prompt_history" not in st.session_state:
-    st.session_state.user_prompt_history = []
-if "chat_answers_history" not in st.session_state:
-    st.session_state.chat_answers_history = []
+if (
+    "user_prompt_history" not in st.session_state
+    and "chat_answers_history" not in st.session_state
+    and "chat_history" not in st.session_state
+):
+    st.session_state["user_prompt_history"] = []
+    st.session_state["chat_answers_history"] = []
+    st.session_state["chat_history"] = []
 
 
 def create_sources_str(source_urls: set[str]) -> str:
@@ -23,13 +27,15 @@ def create_sources_str(source_urls: set[str]) -> str:
 
 if prompt:
     with st.spinner("Thinking..."):
-        response = run_llm(query=prompt)
+        response = run_llm(query=prompt, chat_history=st.session_state.chat_history)
         sources = set([doc.metadata["source"] for doc in response["source_documents"]])
 
         formatted_response = f"{response["result"]}\n\n {create_sources_str(sources)}"
 
-        st.session_state.user_prompt_history.append(prompt)
-        st.session_state.chat_answers_history.append(formatted_response)
+        st.session_state["user_prompt_history"].append(prompt)
+        st.session_state["chat_answers_history"].append(formatted_response)
+        st.session_state["chat_history"].append(("human", prompt))
+        st.session_state["chat_history"].append(("ai", response["result"]))
 
 if st.session_state.chat_answers_history:
     for response, user_query in zip(
